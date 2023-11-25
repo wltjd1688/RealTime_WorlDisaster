1ec539070fd13f90e1f6ee93b7dd8eb51001a06c
wltjd1688 <wltjd1688@gmail.com>
2023-11-26 00:17:55 +0900

    [update] 클러스터링 로직만 추가

--- src/app/components/EarthCesium.tsx
+++ src/app/components/EarthCesium.tsx
@@ -1,9 +1,11 @@
 "use client"
 import React, { useState, useEffect, useRef } from 'react';
-import { Viewer, Math, Cartesian3, Color } from 'cesium';
+import {Viewer, Math, Cartesian3, Color, PinBuilder, VerticalOrigin, EntityCluster, Ion} from 'cesium';
 import { useRouter } from 'next/navigation';
 import 'cesium/Build/Cesium/Widgets/widgets.css';
 import axios from 'axios';
+
+Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzZmExZTg0My0yY2RiLTQ0NDEtYTg1Mi03OTVlZDk4NDA3NGEiLCJpZCI6MTgwMTk3LCJpYXQiOjE3MDA3Mjc4NjZ9.DkUl80JHl3OeYlUA6VMWyyGbR1tc4N1seqN8jmnlMkI";
 
 interface disasterInfo {
   dId: number;
@@ -61,7 +63,7 @@ const EarthCesium = () => {
         return Color.DARKSLATEGRAY; // 강한 폭풍을 어두운 회색으로 표현
       case 'Extratropical Cyclone':
         return Color.DARKORCHID; // 외대륙 사이클론을 어두운 보라색으로 표현
-      case '"Heat Wave”':
+      case 'Heat Wave':
         return Color.RED; // 열파를 빨간색으로 표현      
       default:
         return Color.WHITE;
@@ -72,23 +74,51 @@ const EarthCesium = () => {
   useEffect(() => {
     if (typeof window !== 'undefined' && cesiumContainer.current) {
       viewer = new Viewer(cesiumContainer.current,{
-                // 여기에 옵션을 설정합니다.
-                animation: false,  // 애니메이션 위젯 비활성화
-                baseLayerPicker: false,  // 베이스 레이어 선택기 비활성화
-                fullscreenButton: false,  // 전체 화면 버튼 비활성화
-                vrButton: false,  // VR 버튼 비활성화
-                geocoder: false,  // 지오코더 비활성화
-                homeButton: true,  // 홈 버튼 비활성화
-                infoBox: false,  // 정보 박스 비활성화
-                sceneModePicker: false,  // 장면 모드 선택기 비활성화
-                selectionIndicator: false,  // 선택 지시기 비활성화
-                timeline: false,  // 타임라인 비활성화
-                navigationHelpButton: false,  // 네비게이션 도움말 버튼 비활성화
-                // 추가적인 옵션들...
+        // 여기에 옵션을 설정합니다.
+        animation: false,  // 애니메이션 위젯 비활성화
+        baseLayerPicker: false,  // 베이스 레이어 선택기 비활성화
+        fullscreenButton: false,  // 전체 화면 버튼 비활성화
+        vrButton: false,  // VR 버튼 비활성화
+        geocoder: false,  // 지오코더 비활성화
+        homeButton: true,  // 홈 버튼 비활성화
+        infoBox: false,  // 정보 박스 비활성화
+        sceneModePicker: false,  // 장면 모드 선택기 비활성화
+        selectionIndicator: false,  // 선택 지시기 비활성화
+        timeline: false,  // 타임라인 비활성화
+        navigationHelpButton: false,  // 네비게이션 도움말 버튼 비활성화
+        // 추가적인 옵션들...
       });
-    };
+
+    // 클러스터링 설정
+    viewer.entities.cluster = new EntityCluster({
+      enabled: true,
+      pixelRange: 15,
+      minimumClusterSize: 3
+    });
+
+    // 클러스터링 이벤트 핸들러 설정
+    viewer.entities.cluster.clusterEvent.addEventListener((clusteredEntities:any, cluster:any) => {
+      cluster.label.show = true;
+      cluster.billboard.show = true;
+      cluster.billboard.verticalOrigin = VerticalOrigin.BOTTOM;
       
-      const axiosData = async () => {
+      const pinbuilder = new PinBuilder();
+  
+      const singleDigitPins = new Array(8);
+      for (let i = 0; i < singleDigitPins.length; ++i) {
+        singleDigitPins[i] = pinbuilder.fromText(`${i+2}`, Color.VIOLET, 48).toDataURL();
+      }
+
+      const count = clusteredEntities.length;
+      if (count < 10){
+        cluster.billboard.image = singleDigitPins[count - 2];
+      } else {
+        cluster.billboard.image = pinbuilder.fromText("10+", getColorForDisasterType(''), 48).toDataURL();
+      }
+    });
+
+      
+      const loadData = async () => {
         try{
           const res = await axios('https://worldisaster.com/api/oldDisasters');
           const data = await res.data;
@@ -98,8 +128,11 @@ const EarthCesium = () => {
             let longitude = item.dCountryLongitude;
             viewer.entities.add({
               position: Cartesian3.fromDegrees(longitude, latitude),
+              // billboard: {
+              //   image: pinImage.fromText(`${item.dType}`,getColorForDisasterType(item.dType), 96).toDataURL(),
+              // },
               point: {
-                pixelSize: 10,
+                pixelSize: 20,
                 color: getColorForDisasterType(item.dType),
               },
               label: {
@@ -116,7 +149,7 @@ const EarthCesium = () => {
         }
       }
 
-      axiosData();
+      loadData();
 
       viewer.camera.moveEnd.addEventListener(() => {
         const cartographicPosition = viewer.camera.positionCartographic;
@@ -138,7 +171,8 @@ const EarthCesium = () => {
         viewer.destroy();
       }
     };
-  },[router]);
+  }
+},[router]);
 
   return (
     <div id="cesiumContainer" ref={cesiumContainer}></div>
