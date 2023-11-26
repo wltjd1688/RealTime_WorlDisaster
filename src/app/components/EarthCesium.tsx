675b00405d756af8520e3c3b3718d4e42800b346
wltjd1688 <wltjd1688@gmail.com>
2023-11-27 01:54:34 +0900

    [update] 클러스터링

--- src/app/components/EarthCesium.tsx
+++ src/app/components/EarthCesium.tsx
@@ -1,28 +1,33 @@
 "use client"
 import React, { useState, useEffect, useRef } from 'react';
-import {Viewer, Math, Cartesian3, Color, PinBuilder, VerticalOrigin, EntityCluster,IonWorldImageryStyle, IonImageryProvider, ImageryLayer, createWorldImageryAsync} from 'cesium';
+import {Viewer, Math, Cartesian3, Color, PinBuilder, EntityCluster ,IonWorldImageryStyle, createWorldImageryAsync, CustomDataSource} from 'cesium';
 import { useRouter } from 'next/navigation';
 import 'cesium/Build/Cesium/Widgets/widgets.css';
 import axios from 'axios';
-
 
 // Ion.defaultAccessToken = "";
 
 interface disasterInfo {
   dId: number;
   dType: string;
-  dCountry: string;
+  d: string;
   dStatus: string;
   dDate: string;
   dLatitude: number|null;
   dLongitude: number|null;
-  dCountryLatitude: number;
-  dCountryLongitude: number;
+  dLatitude: number;
+  dLongitude: number;
+}
+
+interface EntityClusterExtension {
+  cluster: EntityCluster;
 }
 
 const EarthCesium = () => {
   const cesiumContainer = useRef(null);
   const router = useRouter();
+  const viewerRef = useRef<Viewer|null>(null);
+
   function getColorForDisasterType(type:any) {
     switch (type) {
       case "Tropical Cyclone":
@@ -69,11 +74,10 @@ const EarthCesium = () => {
         return Color.WHITE;
     }
   }
-  
 
   useEffect(() => {
     if (typeof window !== 'undefined' && cesiumContainer.current) {
-      viewer = new Viewer(cesiumContainer.current,{
+      let viewer = new Viewer(cesiumContainer.current,{
         animation: false,  // 애니메이션 위젯 비활성화
         baseLayerPicker: false,  // 베이스 레이어 선택기 비활성화
         fullscreenButton: false,  // 전체 화면 버튼 비활성화
@@ -85,7 +89,6 @@ const EarthCesium = () => {
         selectionIndicator: false,  // 선택 지시기 비활성화
         timeline: false,  // 타임라인 비활성화
         navigationHelpButton: false,  // 네비게이션 도움말 버튼 비활성화
-        // 이거 외에 옵션들
         // navigationInstructionsInitiallyVisible?: boolean;
         // scene3DOnly?: boolean;
         // shouldAnimate?: boolean;
@@ -123,10 +126,66 @@ const EarthCesium = () => {
         // depthPlaneEllipsoidOffset?: number;
         // msaaSamples?: number;
       });
-    }
 
-    // cluster 생성
-    const pinBuilder = new PinBuilder();
+    viewerRef.current = viewer;  
+
+    // layout 추가
+    createWorldImageryAsync({
+      style: IonWorldImageryStyle.AERIAL_WITH_LABELS
+    }).then((imageryProvider) => {
+      viewer.scene.imageryLayers.addImageryProvider(imageryProvider);
+      console.log(`layout추가 성공`)
+    }).catch((err) => {
+      console.log(`layout추가 실패: ${err}`);
+    });
+
+    // viewer 정리 로직 추가
+    return () => {
+      if (viewer && viewer.destroy) {
+        viewer.destroy();        
+      }
+    };
+  }
+},[]);
+
+useEffect(() => {
+  const viewer = viewerRef.current;
+  if(!viewer || !viewer.scene || !viewer.camera) {
+    return;
+  };
+
+  const customDataSource = new CustomDataSource('Disasters');
+
+  customDataSource.clustering = new EntityCluster({
+    enabled: true,
+    pixelRange: 20,
+    minimumClusterSize: 3,
+    clusterBillboards: true,
+    clusterLabels: true,
+    clusterPoints: true,
+  })
+
+  customDataSource.clustering.clusterEvent.addEventListener((clusteredEntities, cluster) => {
+    let count = clusteredEntities.length;
+
+    if (count >= 50) {
+      cluster.billboard.image = pin50;
+    } else if (count >= 40) {
+      cluster.billboard.image = pin40;
+    } else if (count >= 30) {
+      cluster.billboard.image = pin30;
+    } else if (count >= 20) {
+      cluster.billboard.image = pin20;
+    } else if (count >= 10) {
+      cluster.billboard.image = pin10;
+    } else if (count >= 5) {
+      cluster.billboard.image = pin5;
+    } else {
+      cluster.billboard.image = singleDigitPins[count];
+    }
+  })
+
+  const pinBuilder = new PinBuilder();
     const pin50 = pinBuilder.fromText('50+', Color.RED, 48).toDataURL();
     const pin40 = pinBuilder.fromText('40+', Color.ORANGE, 48).toDataURL();
     const pin30 = pinBuilder.fromText('30+', Color.YELLOW, 48).toDataURL();
@@ -137,76 +196,62 @@ const EarthCesium = () => {
     for (let i = 0; i < singleDigitPins.length; ++i) {
       singleDigitPins[i] = pinBuilder.fromText(String(i), Color.VIOLET, 48).toDataURL();
     }
-    
 
+  // cluster pinBuild
+  const loadData = async (viewer:Viewer) => {
+    const pinimage = new PinBuilder();
+    try{
+      const res = await axios('https://worldisaster.com/api/oldDisasters');
+      const data = await res.data;
 
-
-    // 데이터 가져오기 및 point 생성
-    const loadData = async () => {
-      try{
-        const pinImage = new PinBuilder();
-        const res = await axios('https://worldisaster.com/api/oldDisasters');
-        const data = await res.data;
-        data.forEach((item:disasterInfo,index:number)=>{
-          if (typeof item.dCountryLatitude === 'number' && typeof item.dCountryLongitude === 'number'){
-          let latitude = item.dCountryLatitude;
-          let longitude = item.dCountryLongitude;
-          viewer.entities.add({
-            // 데이터 좌표 넣기
-            position: Cartesian3.fromDegrees(longitude, latitude),
-            // 표지판 이미지
-            billboard: {
-              image: pinImage.fromColor(getColorForDisasterType(item.dType), 48).toDataURL(),
-            },
-            // 포인트 이미지
-            // point: {
-            //   pixelSize: 20,
-            //   color: getColorForDisasterType(item.dType),
-            // },
-            label: {
-              Type: item.dType,
-              country: item.dCountry,
-              status: item.dStatus,
-              data: item.dDate
-            },
-          });
-          }
+      data.forEach((item:disasterInfo)=>{
+        if (typeof item.dLatitude === 'number' && typeof item.dLongitude === 'number'){
+        let latitude = item.dLatitude;
+        let longitude = item.dLongitude;
+        customDataSource.entities.add({
+          // 데이터 좌표 넣기
+          position: Cartesian3.fromDegrees(longitude, latitude),
+          // 표지판 이미지
+          billboard: {
+            image: pinimage.fromText(`${1}`,getColorForDisasterType(item.dType), 48).toDataURL(), // 표지판 이미지
+          },
+          // 포인트 이미지
+          // point: {
+          //   pixelSize: 20,
+          //   color: getColorForDisasterType(item.dType),
+          // },
         });
-        console.log(`데이터 가져오기 성공`)  
-      } catch (error) {
-        console.log(`데이터 가져오기 실패: ${error}`)
-      }
+        }
+      });
+      console.log(`데이터 로드 성공`);
+      console.log(data)
+    } catch(err) {
+      console.log('데이터 로드 실패', err);
     }
+  }
+  loadData(viewer);
 
-    // 만든함수 실행
-    loadData();
-    
-    // 카메라 이동시 uri에 표시되는 좌표값 변경
-    viewer.camera.moveEnd.addEventListener(() => {
+  viewer.dataSources.add(customDataSource);
+
+},[]);
+
+useEffect(() => {
+  const viewer = viewerRef.current;
+  if(!viewer || !viewer.scene || !viewer.camera) {
+    return;
+  };
+
+  const moveEndListener = viewer.camera.moveEnd.addEventListener(() => {
       const cartographicPosition = viewer.camera.positionCartographic;
       const longitude = Math.toDegrees(cartographicPosition.longitude).toFixed(6);
       const latitude = Math.toDegrees(cartographicPosition.latitude).toFixed(6);
       router.push(`/earth?lon=${longitude}&lat=${latitude}`, undefined);
-    });
+  });
 
-    // layout 추가
-    createWorldImageryAsync({
-      style: IonWorldImageryStyle.AERIAL_WITH_LABELS
-    }).then((imageryProvider) => {
-      viewer.scene.imageryLayers.addImageryProvider(imageryProvider);
-      console.log(`layout추가 성공`)
-    }).catch((err) => {
-      console.log(`layout추가 실패: ${err}`);
-    }
-    );
-
-    // viewer 정리 로직 추가
-    return () => {
-      if (viewer && viewer.destroy) {
-        viewer.destroy();
-      }
-    };
-},[router]);
+  return () => {
+    moveEndListener();
+  };
+}, [router]);
 
   return (
     <div id="cesiumContainer" ref={cesiumContainer}></div>
