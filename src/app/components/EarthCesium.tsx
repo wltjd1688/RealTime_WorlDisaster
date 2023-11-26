e1c8df6c0888483371426cd550807aee507938e1
wltjd1688 <wltjd1688@gmail.com>
2023-11-26 15:45:18 +0900

    [update] layout 띄우기

--- src/app/components/EarthCesium.tsx
+++ src/app/components/EarthCesium.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState, useEffect, useRef } from 'react';
-import {Viewer, Math, Cartesian3, Color, PinBuilder, EntityCluster ,IonWorldImageryStyle, createWorldImageryAsync, CustomDataSource} from 'cesium';
+import {Viewer, Math, Cartesian3, Color, PinBuilder, VerticalOrigin, EntityCluster, Ion} from 'cesium';
 import { useRouter } from 'next/navigation';
 import 'cesium/Build/Cesium/Widgets/widgets.css';
 import axios from 'axios';
@@ -93,59 +93,16 @@ const EarthCesium = () => {
         selectionIndicator: false,  // 선택 지시기 비활성화
         timeline: false,  // 타임라인 비활성화
         navigationHelpButton: false,  // 네비게이션 도움말 버튼 비활성화
-        // navigationInstructionsInitiallyVisible?: boolean;
-        // scene3DOnly?: boolean;
-        // shouldAnimate?: boolean;
-        // clockViewModel?: ClockViewModel;
-        // selectedImageryProviderViewModel?: ProviderViewModel;
-        // imageryProviderViewModels?: ProviderViewModel[];
-        // selectedTerrainProviderViewModel?: ProviderViewModel;
-        // terrainProviderViewModels?: ProviderViewModel[];
-        // baseLayer?: ImageryLayer | false;
-        // terrainProvider?: TerrainProvider;
-        // terrain?: Terrain;
-        // skyBox?: SkyBox | false;
-        // skyAtmosphere?: SkyAtmosphere | false;
-        // fullscreenElement?: Element | string;
-        // useDefaultRenderLoop?: boolean;
-        // targetFrameRate?: number;
-        // showRenderLoopErrors?: boolean;
-        // useBrowserRecommendedResolution?: boolean;
-        // automaticallyTrackDataSourceClocks?: boolean;
-        // contextOptions?: ContextOptions;
-        // sceneMode?: SceneMode;
-        // mapProjection?: MapProjection;
-        // globe?: Globe | false;
-        // orderIndependentTranslucency?: boolean;
-        // creditContainer?: Element | string;
-        // creditViewport?: Element | string;
-        // dataSources?: DataSourceCollection;
-        // shadows?: boolean;
-        // terrainShadows?: ShadowMode;
-        // mapMode2D?: MapMode2D;
-        // projectionPicker?: boolean;
-        // blurActiveElementOnCanvasFocus?: boolean;
-        // requestRenderMode?: boolean;
-        // maximumRenderTimeChange?: number;
-        // depthPlaneEllipsoidOffset?: number;
-        // msaaSamples?: number;
+        // 추가적인 옵션들...
       });
 
-    viewerRef.current = viewer;  
-
-    // layout 추가
-    createWorldImageryAsync({
-      style: IonWorldImageryStyle.AERIAL_WITH_LABELS
-    }).then((imageryProvider) => {
-      viewer.scene.imageryLayers.addImageryProvider(imageryProvider);
-      console.log(`layout추가 성공`)
-    }).catch((err) => {
-      console.log(`layout추가 실패: ${err}`);
+    // 클러스터링 설정
+    viewer.entities.cluster = new EntityCluster({
+      enabled: true,
+      pixelRange: 15,
+      minimumClusterSize: 3
     });
 
-<<<<<<< HEAD
-    // viewer 정리 로직 추가
-=======
     // 클러스터링 이벤트 핸들러 설정
     viewer.entities.cluster.clusterEvent.addEventListener((clusteredEntities:any, cluster:any) => {
       cluster.label.show = true;
@@ -218,112 +175,7 @@ const EarthCesium = () => {
       }
     };
   }
-},[]);
-
-useEffect(() => {
-  const viewer = viewerRef.current;
-  if(!viewer || !viewer.scene || !viewer.camera) {
-    return;
-  };
-
-  const customDataSource = new CustomDataSource('Disasters');
-
-  customDataSource.clustering = new EntityCluster({
-    enabled: true,
-    pixelRange: 20,
-    minimumClusterSize: 3,
-    clusterBillboards: true,
-    clusterLabels: true,
-    clusterPoints: true,
-  })
-
-  customDataSource.clustering.clusterEvent.addEventListener((clusteredEntities, cluster) => {
-    let count = clusteredEntities.length;
-
-    if (count >= 50) {
-      cluster.billboard.image = pin50;
-    } else if (count >= 40) {
-      cluster.billboard.image = pin40;
-    } else if (count >= 30) {
-      cluster.billboard.image = pin30;
-    } else if (count >= 20) {
-      cluster.billboard.image = pin20;
-    } else if (count >= 10) {
-      cluster.billboard.image = pin10;
-    } else if (count >= 5) {
-      cluster.billboard.image = pin5;
-    } else {
-      cluster.billboard.image = singleDigitPins[count];
-    }
-  })
-
-  const pinBuilder = new PinBuilder();
-    const pin50 = pinBuilder.fromText('50+', Color.RED, 48).toDataURL();
-    const pin40 = pinBuilder.fromText('40+', Color.ORANGE, 48).toDataURL();
-    const pin30 = pinBuilder.fromText('30+', Color.YELLOW, 48).toDataURL();
-    const pin20 = pinBuilder.fromText('20+', Color.GREEN, 48).toDataURL();
-    const pin10 = pinBuilder.fromText('10+', Color.BLUE, 48).toDataURL();
-    const pin5 = pinBuilder.fromText('5+', Color.PURPLE, 48).toDataURL();
-    const singleDigitPins = new Array(10);
-    for (let i = 0; i < singleDigitPins.length; ++i) {
-      singleDigitPins[i] = pinBuilder.fromText(String(i), Color.VIOLET, 48).toDataURL();
-    }
-
-  // cluster pinBuild
-  const loadData = async (viewer:Viewer) => {
-    const pinimage = new PinBuilder();
-    try{
-      const res = await axios('https://worldisaster.com/api/oldDisasters');
-      const data = await res.data;
-
-      data.forEach((item:disasterInfo)=>{
-        if (typeof item.dLatitude === 'number' && typeof item.dLongitude === 'number'){
-        let latitude = item.dLatitude;
-        let longitude = item.dLongitude;
-        customDataSource.entities.add({
-          // 데이터 좌표 넣기
-          position: Cartesian3.fromDegrees(longitude, latitude),
-          // 표지판 이미지
-          billboard: {
-            image: pinimage.fromText(`${1}`,getColorForDisasterType(item.dType), 48).toDataURL(), // 표지판 이미지
-          },
-          // 포인트 이미지
-          // point: {
-          //   pixelSize: 20,
-          //   color: getColorForDisasterType(item.dType),
-          // },
-        });
-        }
-      });
-      console.log(`데이터 로드 성공`);
-      console.log(data)
-    } catch(err) {
-      console.log('데이터 로드 실패', err);
-    }
-  }
-  loadData(viewer);
-
-  viewer.dataSources.add(customDataSource);
-
-},[]);
-
-useEffect(() => {
-  const viewer = viewerRef.current;
-  if(!viewer || !viewer.scene || !viewer.camera) {
-    return;
-  };
-
-  const moveEndListener = viewer.camera.moveEnd.addEventListener(() => {
-      const cartographicPosition = viewer.camera.positionCartographic;
-      const longitude = Math.toDegrees(cartographicPosition.longitude).toFixed(6);
-      const latitude = Math.toDegrees(cartographicPosition.latitude).toFixed(6);
-      router.push(`/earth?lon=${longitude}&lat=${latitude}`, undefined);
-  });
-
-  return () => {
-    moveEndListener();
-  };
-}, [router]);
+},[router]);
 
   return (
     <div id="cesiumContainer" ref={cesiumContainer}></div>
